8d05343510074d44b83c3962faf1ba5833d4db47
AaronAPerez <aaperez06@gmail.com>
2024-04-01 21:41:34 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,47 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet("/mathSum", (int a = 1, int b = 2) =>
+{
+    return $"{a} + {b} = {a+b}";
+});
+
+
+app.MapGet("/nameWakeUpTime", (string name = "User", string time = "userInput", string amPm = "amPm") =>
+{
+    return $"Hello {name}, you woke up at {time}{amPm}, Great Job!!!!!";
+});
+
+
+app.MapGet("/compareNumbers", (int a = 1, int b = 2) =>
+{
+    if (a > b){
+        return $"First number {a} is greater than second number {b}\nSecond number {b} is less than first number {a}";
+    }
+    else if(a < b){
+        return $"First number {a} is less than second number {b}\nSecond number {b} is greater than first number {a}";
+    }
+    else{
+        return $"First number {a} and second number {b} are EQUAL";
+    }
+
+});
+
+
+app.Run();
+
